f91804f2d45014681394773322f3fa69b61ffdd7
Stevie <xxlennygamerxx@gmail.com>
2020-08-02 18:54:07 -0700

    Create GlobalNerfs.cs

---
+++ GlobalNerfs.cs
@@ -0,0 +1,27 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+
+namespace ConfigurableNerfs
+{
+    public class GlobalItemNerfs : GlobalItem
+    {
+        public override void SetDefaults(Item item)
+        {
+            switch (item.type)
+            {
+
+            }
+        }
+    }
+
+    public class GlobalNPCNerfs : GlobalNPC
+    {
+        public override void SetDefaults(NPC npc)
+        {
+            switch (npc.type)
+            {
+
+            }
+        }
+    }
+}
